b90d7de3494e0b9cda7ddecaed256ee541ea6535
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2024-03-29 18:11:16 -0300

    first commit

---
+++ src/main/java/com/springbootapp/SpringRameshApplication.java
@@ -0,0 +1,13 @@
+package com.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRameshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRameshApplication.class, args);
+	}
+
+}
